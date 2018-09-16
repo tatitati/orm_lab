0770f1550e08a1bb067178104f2586a6419f1925
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-09-16 21:50:23 +0100

    [INFR] - Added User entity

---
+++ src/Entity/PersistenceModel/User.php
@@ -0,0 +1,22 @@
+<?php
+Namespace App\Entity\Infrastructure;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="UserRepository")
+ * @ORM\Table(name="user")
+ */
+class User
+{
+    /**
+     * @var int
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+        /** @ORM\Column(type="string", name="text") **/
+    private $name;
+}
